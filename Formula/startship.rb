1389a8b99a49bc34976dac42bac82e327429a6a0
Saket Narayan <saket@saket.me>
2021-01-26 15:09:41 -0500

    Update startship.rb

--- Formula/startship.rb
+++ Formula/startship.rb
@@ -2,8 +2,8 @@ class Startship < Formula
   desc "Automate releasing of staged artifacts on Sonatype Nexus"
   homepage "https://github.com/saket/startship"
   url "https://github.com/saket/startship/releases/download/1.1.0/startship.zip"
-  version "1.1.0"
-  sha256 "9849b9ef8b2c303bee20750650d911f2aec21cfff5a2e35f9840ea2cc1503463"
+  version "1.1.1"
+  sha256 "be4cd615ee5f4c0792ffea45baed2a7894fd79c5b4b91b05a1c895b4ef27359d"
 
   bottle :unneeded
 
