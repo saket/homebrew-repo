af579bcfe9adb7074084cb8234c7f23aedab1129
Saket Narayan <saket@saket.me>
2022-08-11 01:27:36 -0400

    Release unfurl 1.7.0
    https://github.com/saket/unfurl/releases/tag/1.7.0

--- Formula/unfurl.rb
+++ Formula/unfurl.rb
@@ -1,9 +1,9 @@
 class Unfurl < Formula
   desc "Generate preview of links, inspired by Slack"
   homepage "https://github.com/saket/unfurl"
-  url "https://github.com/saket/unfurl/releases/download/1.6.0/unfurl-cli.zip"
-  version "1.6.0"
-  sha256 "6ebbc83d9d108c404a7371dcc654759d605d84ebb27fca959a78721e7bbbf996"
+  url "https://github.com/saket/unfurl/releases/download/1.7.0/unfurl-cli.zip"
+  version "1.7.0"
+  sha256 "a78f873ce3c261695bdae37caac9b5a49b52b3366111b7d35411eebd4b17b397"
 
   depends_on "openjdk"
 
