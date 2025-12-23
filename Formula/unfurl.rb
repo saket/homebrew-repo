363040a82415a28960c6a59460ca480ba7ddedef
Saket Narayan <saket@saket.me>
2025-12-22 22:48:46 -0500

    Update Unfurl to version 2.3.1

--- Formula/unfurl.rb
+++ Formula/unfurl.rb
@@ -1,9 +1,9 @@
 class Unfurl < Formula
   desc "Generate rich preview of links, inspired by Slack"
   homepage "https://github.com/saket/unfurl"
-  url "https://github.com/saket/unfurl/releases/download/2.3.0/unfurl-cli.zip"
-  version "2.3.0"
-  sha256 "d68a6c4da7d5e2a6941020d1cc7fb15cf03884c5997f56421d63193db079c9c5"
+  url "https://github.com/saket/unfurl/releases/download/2.3.0/unfurl-cli-2.3.1.zip"
+  version "2.3.1"
+  sha256 "0c41c270cf3f80fe83133e7ed2e9df6bc985a79be1d8366bb43d6d746777cb95"
 
   depends_on "openjdk"
 
